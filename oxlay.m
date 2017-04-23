88a21b60b682a382149e8a1667d37ae8563729f9
Surendrajat <sk2812283@gmail.com>
2017-04-23 11:36:39 +0530

    updated

--- oxlay.m
+++ oxlay.m
@@ -1,5 +1,9 @@
-function opt_delta = oxlay (V, t1, w, Cp, K)
+function [Fc, V] = oxlay (D, N, t_input, DoC)
 %% flow chart implementation
+
+V = pi*D*N/60000;
+t1 = t_input/1000;
+w = 1.402*DoC/1000;
 
 A = 553.1; B = 600.8;
 alpha = -5;
@@ -8,20 +12,25 @@ m = 1;
 n = 1.45;
 Tw = 313;
 ro = 7580;
+Tm = 2600;
 
 ita = 0:0.1:1;
 psi = 0:0.1:1;
 sigma_diff = 1e5;            % assumed init. diff. between sigmaN & N_
 opt_C0 = 2;                  % assumed optimum C0
+diff = 1e5;
+opt_phi = 5;
+
+E_0 = 1;
 
 for delta = 0.005:0.005:0.2
-    itr = 1;
+    Fc_min_m = [];
     for C0 = 2:0.1:10
+        Fc_min = [];
+        Fc = [];
+
         for phi = 5:0.1:45
-            to_int = zeros(size(phi));
-            k_chip = zeros(size(phi));
-            Fc = zeros(size(phi));
-            
+            itr = 1;
             l = t1/sind(phi);
             Vs = V*cosd(alpha)/cosd(phi-alpha);
             GammaAB = 0.5*cos(alpha)/sin(phi)/cos(phi-alpha);
@@ -33,7 +42,7 @@ for delta = 0.005:0.005:0.2
                 Cp = 420 + 0.504*Tab;
                 K = 52.61 - 0.0281*Tab;
                 k_ab = sqrt(1/3)*(A+B*Eab^n)...
-                    *(1+C*log(E_ab/E_0)*(1-((Tab-Tw)/(tm-tw))^m));
+                    *(1+C*log(E_ab/E_0)*(1-((Tab-Tw)/(Tm-Tw))^m));
                 Fs = k_ab*l*w;
                 Rt = ro*Cp*V*t1/K;
                 
@@ -64,7 +73,7 @@ for delta = 0.005:0.005:0.2
             Vc = V*sind(phi)/cosd(phi-alpha);
             h = t1*sind(theta)/cosd(lambda)/sin(phi)*...
                 (1+ (C0*n_eq/( 3*(1+2*(pi/4-phi)-C0*n_eq) )) );
-            to_int(itr) = F/h/w;
+            to_int = F/h/w;
             GammaM = h/delta/t2;
             GammaInt = 2*GammaAB + 0.5*GammaM;
             Eint = GammaInt/sqrt(3);
@@ -73,8 +82,8 @@ for delta = 0.005:0.005:0.2
             while true
                 Cp = 420 + 0.504*Tc;
                 K = 52.61 - 0.0281*Tc;
-                del_Tc = F*Vc/m_chip/Cp;
-                NewTc = Tw + Tsz + del_Tc;
+                delta_Tc = F*Vc/m_chip/Cp;
+                NewTc = Tw + Tsz + delta_Tc;
                 if abs(NewTc - Tc) > 0.1
                     Tc = NewTc;
                 else
@@ -85,16 +94,17 @@ for delta = 0.005:0.005:0.2
             del_Tm = delta_Tc*...
                 10^(0.06-0.195*delta*sqrt(Rt*t2/h)+0.5*log10(Rt*t2/h));
             Tint = Tw + Tsz + mean(psi*del_Tm);
-            k_chip(itr) = sqrt(1/3)*(A+B*Eint^n)...
-                *(1+C*log(E_int/E_0)*(1-((Tint-Tw)/(tm-tw))^m));
+            k_chip = sqrt(1/3)*(A+B*Eint^n)...
+                *(1+C*log(E_int/E_0)*(1-((Tint-Tw)/(Tm-Tw))^m));
+            
+            % computing optimum phi
+            if abs(to_int - k_chip) < diff
+                opt_phi = phi;
+                diff = abs(to_int - k_chip);
+            end
+%             disp(opt_phi);
         end
         Fc_min = [Fc_min min(Fc)];
-        % coomputing optimum phi
-        diff = abs(to_int - k_chip);
-        phi_index = logical(diff==min(diff((5:0.1:40)*10-50)));             %not sure
-        fi = 5:0.1:45;
-        opt_phi = fi(phi_index);
-        disp(opt_phi);
         
         sigmaN_ = k_ab*(1+pi/2-2*alpha-2*C0*n_eq);
         sigmaN = N/h/w; 
@@ -106,12 +116,7 @@ for delta = 0.005:0.005:0.2
         end
     end
     Fc_min_m = [Fc_min_m min(Fc_min)];
-    disp(opt_C0);
+%     disp(opt_C0);
     
-    new_Fc = [newFc exp];                   %no idea
 end
-%computing optimum delta
-del_index = logical(new_Fc==min(new_FC));
-delta = 0.005:0.005:0.2;
-opt_delta = delta(del_index);
-disp(opt_delta);
+disp(min(Fc_min_m));
